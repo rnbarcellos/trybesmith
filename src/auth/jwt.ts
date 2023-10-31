4ba1c6453a48b4da10a969506438f9a7c2fd149a
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-10-30 21:00:51 -0300

    :recycle: refactor: removes jwt expire time

--- src/auth/jwt.ts
+++ src/auth/jwt.ts
@@ -3,11 +3,7 @@ import { Payload } from '../types/Payload';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'jwt_secret';
 
-const config = {
-  expiresIn: '1d',
-};
-
-const sign = (payload: Payload): string => jwt.sign(payload, JWT_SECRET, config);
+const sign = (payload: Payload): string => jwt.sign(payload, JWT_SECRET);
 
 const verify = (token: string): Payload => jwt.verify(token, JWT_SECRET) as Payload;
 
